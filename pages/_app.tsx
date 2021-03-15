12fb9ac18f86c78f5c51c91af3e37575fa5dac70
파랑이 <parangee9706@gmail.com>
2021-03-15 18:01:26 +0900

    feat: layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
 import { NextComponentType } from 'next';
 import { AppContext, AppInitialProps, AppProps } from 'next/app';
+import Layout from '@components/Layout';
 
 const MyApp: NextComponentType<AppContext, AppInitialProps, AppProps> = ({
   Component,
   pageProps,
 }) => {
   return (
-    <>
+    <Layout>
       <Component {...pageProps} />
-    </>
+    </Layout>
   );
 };
 
