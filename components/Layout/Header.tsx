f96908f7b65c59a4a4cf6cd09e918464361821d2
파랑이 <parangee9706@gmail.com>
2021-03-15 18:14:46 +0900

    fixed top

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -3,7 +3,7 @@ import { Navbar, NavbarBrand } from 'react-bootstrap-v5'
 
 const Header = () => {
   return (
-    <Navbar bg="primary" variant="dark">
+    <Navbar bg="primary" variant="dark" fixed="top">
       <div className="container-fluid">
         <NavbarBrand>큐새</NavbarBrand>
       </div>
