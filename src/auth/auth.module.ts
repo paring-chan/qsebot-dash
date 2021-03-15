fccee9578bb5dcc681a309ebae1c6792422f7af5
파랑이 <parangee9706@gmail.com>
2021-03-15 21:17:08 +0900

    feat: jwt module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,16 @@ import { AuthService } from './auth.service'
 import { UsersModule } from '../users/users.module'
 import { LocalStrategy } from './local.strategy'
 import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt'
 
 @Module({
   providers: [AuthService, LocalStrategy],
-  imports: [UsersModule, PassportModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+    }),
+  ],
 })
 export class AuthModule {}
