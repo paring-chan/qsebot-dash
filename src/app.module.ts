4c3a4d6cadf5a042f2c425624bed9d5d73b84131
파랑이 <parangee9706@gmail.com>
2021-03-15 21:30:54 +0900

    feat: apply auth middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common'
+import { MiddlewareConsumer, Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { RenderModule } from 'nest-next'
 import { AuthModule } from './auth/auth.module'
 import { UsersModule } from './users/users.module'
 import Next from 'next'
+import { AuthMiddleware } from './auth/auth.middleware'
 
 @Module({
   imports: [
@@ -17,4 +18,8 @@ import Next from 'next'
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware)
+  }
+}
