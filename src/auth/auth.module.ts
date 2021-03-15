2128dc30ac4b8e2875c0532f96fed8a8b8ab0ace
파랑이 <parangee9706@gmail.com>
2021-03-15 20:49:00 +0900

    add local strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { UsersModule } from '../users/users.module'
+import { LocalStrategy } from './local.strategy'
 
 @Module({
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
   imports: [UsersModule],
 })
 export class AuthModule {}
