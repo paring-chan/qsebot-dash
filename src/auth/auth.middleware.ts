e5ae902c3d0cf842517c09acffad3c9b53e16c95
파랑이 <parangee9706@gmail.com>
2021-03-15 21:30:05 +0900

    feat: auth middleware

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -4,6 +4,9 @@ import { Request, Response } from 'express'
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: () => void): any {
+    if (!req.path.startsWith('/auth') && !req.user) {
+      return res.redirect('/auth/login')
+    }
     next()
   }
 }
