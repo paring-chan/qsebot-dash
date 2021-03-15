6f7eb03d98862fd53d670ad17befda1a6fc5ee14
파랑이 <parangee9706@gmail.com>
2021-03-15 20:20:32 +0900

    feat: inject UsersService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { UsersService } from '../users/users.service'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+}
