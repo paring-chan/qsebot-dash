a0aa74bfc046e1991b00f1bec23cb8215ef42e1d
파랑이 <parangee9706@gmail.com>
2021-03-15 20:51:18 +0900

    feat: id and password field

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -6,7 +6,10 @@ import { AuthService } from './auth.service'
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
-    super()
+    super({
+      usernameField: 'id',
+      passwordField: 'password',
+    })
   }
 
   async validate(id: string, password: string): Promise<any> {}
