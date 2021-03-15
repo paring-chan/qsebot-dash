913e05dc614584dda381dfa88127f353705615f3
파랑이 <parangee9706@gmail.com>
2021-03-15 20:29:54 +0900

    feat: prettier

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { RenderModule } from 'nest-next'
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module'
+import { UsersModule } from './users/users.module'
 import Next from 'next'
 
 @Module({
