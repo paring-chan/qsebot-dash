89f808b31dd1f88a3db2e987f46f1871c1929ba9
파랑이 <parangee9706@gmail.com>
2021-03-15 17:13:03 +0900

    feat: nextjs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Render } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+  @Render('Index')
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  home() {
+    return {};
   }
 }
