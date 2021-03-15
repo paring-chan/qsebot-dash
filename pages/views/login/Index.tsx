e421ea78f7799ae9375652324ea118de70d687e1
파랑이 <parangee9706@gmail.com>
2021-03-15 21:54:10 +0900

    feat: first login alert

--- pages/views/login/Index.tsx
+++ pages/views/login/Index.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import PageContent from '@components/PageContent'
 import { NextPage, NextPageContext } from 'next'
+import { Alert, Card } from 'react-bootstrap-v5'
 
 const Login: NextPage<any> = ({ query }) => {
-  return <PageContent>{JSON.stringify(query)}</PageContent>
+  return (
+    <PageContent>
+      <Card>
+        <Card.Body>
+          {query.firstRun && (
+            <Alert variant="primary">
+              현재 존재하는 유저가 없습니다. 유저를 생성해주세요.
+            </Alert>
+          )}
+        </Card.Body>
+      </Card>
+    </PageContent>
+  )
 }
 
 Login.getInitialProps = async function (context: NextPageContext) {
