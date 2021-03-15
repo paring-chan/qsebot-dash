d253001dfc04155424644b7f1f37980e17c638e6
파랑이 <parangee9706@gmail.com>
2021-03-15 20:21:06 +0900

    feat: validateUser method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,4 +4,6 @@ import { UsersService } from '../users/users.service'
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
+
+  async validateUser(username: string, password: string): Promise<any> {}
 }
