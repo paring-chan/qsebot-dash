2bd167d07dc753967ce2c1a4b2bd77edd88cce7c
파랑이 <parangee9706@gmail.com>
2021-03-16 17:45:57 +0900

    feat: user dropdown

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
-import { Navbar, NavbarBrand } from 'react-bootstrap-v5'
+import {
+  Dropdown,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavDropdown,
+} from 'react-bootstrap-v5'
 
 const Header = ({ user }: any) => {
   return (
     <Navbar bg="primary" variant="dark" fixed="top">
       <div className="container-fluid">
         <NavbarBrand>큐새</NavbarBrand>
+        <Nav className="d-flex">
+          {user && (
+            <NavDropdown id="nav_user_dropdown" title={user.id}>
+              <Dropdown.Item>로그아웃</Dropdown.Item>
+            </NavDropdown>
+          )}
+        </Nav>
       </div>
     </Navbar>
   )
