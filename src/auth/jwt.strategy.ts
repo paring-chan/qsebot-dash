1a32225c909a5959ed6fa1cb4bb5ffb3540f850a
파랑이 <parangee9706@gmail.com>
2021-03-16 17:35:54 +0900

    feat: parse jwt token from cookie

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@nestjs/common'
 import { PassportStrategy } from '@nestjs/passport'
-import { ExtractJwt, Strategy } from 'passport-jwt'
+import { Strategy } from 'passport-jwt'
+import { IncomingMessage } from 'http'
+import cookie from 'cookie'
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromHeader('token'),
+      jwtFromRequest: (req: IncomingMessage) => {
+        const cookies = cookie.parse(req.headers.cookie || '')
+        return cookies['token']
+      },
       ignoreExpiration: false,
       secretOrKey: process.env.JWT_SECRET,
     })
