b27f4c19481c2c6bb8d3d4d54be8023cd53d15ff
파랑이 <parangee9706@gmail.com>
2021-03-16 20:35:42 +0900

    feat: make items link

--- pages/views/Index.tsx
+++ pages/views/Index.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import PageContent from '@components/PageContent'
 import { ListGroup } from 'react-bootstrap-v5'
+import Link from 'next/link'
 
 const Index = () => {
   return (
     <PageContent>
       <div>
         <ListGroup>
-          <ListGroup.Item action>퀴즈 관리</ListGroup.Item>
-          <ListGroup.Item action>자동응답기(?) 관리</ListGroup.Item>
-          <ListGroup.Item action>관리자 계정 관리</ListGroup.Item>
+          <Link href="/quiz">
+            <ListGroup.Item action>퀴즈 관리</ListGroup.Item>
+          </Link>
+          <Link href="/auto_answer">
+            <ListGroup.Item action>자동응답기(?) 관리</ListGroup.Item>
+          </Link>
+          <Link href="/accounts">
+            <ListGroup.Item action>관리자 계정 관리</ListGroup.Item>
+          </Link>
+          <Link href="/password">
+            <ListGroup.Item action>비밀번호 변경</ListGroup.Item>
+          </Link>
         </ListGroup>
       </div>
     </PageContent>
