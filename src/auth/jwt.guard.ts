e512bf2379521707f4729aad259e4176cd7043d4
파랑이 <parangee9706@gmail.com>
2021-03-16 17:19:02 +0900

    feat: public decorator

--- src/auth/jwt.guard.ts
+++ src/auth/jwt.guard.ts
@@ -1,3 +1,7 @@
 import { AuthGuard } from '@nestjs/passport'
+import { SetMetadata } from '@nestjs/common'
 
 export class JwtGuard extends AuthGuard(['jwt']) {}
+
+export const IS_PUBLIC_KEY = 'isPublic'
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true)
