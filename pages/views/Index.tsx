7437cc92e9e1ec16534dda2d740563f1957c3bd9
파랑이 <parangee9706@gmail.com>
2021-03-16 20:33:15 +0900

    feat: link list

--- pages/views/Index.tsx
+++ pages/views/Index.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import PageContent from '@components/PageContent'
+import { ListGroup } from 'react-bootstrap-v5'
 
 const Index = () => {
-  return <PageContent>Home</PageContent>
+  return (
+    <PageContent>
+      <div>
+        <ListGroup>
+          <ListGroup.Item action>퀴즈 관리</ListGroup.Item>
+          <ListGroup.Item action>자동응답기(?) 관리</ListGroup.Item>
+          <ListGroup.Item action>관리자 계정 관리</ListGroup.Item>
+        </ListGroup>
+      </div>
+    </PageContent>
+  )
 }
 
 export default Index
