c52bff288e7725c06c28ab8920326a8c633333d2
파랑이 <parangee9706@gmail.com>
2021-03-16 17:58:57 +0900

    fix: dropdown menu overflowing the screen

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -4,6 +4,14 @@ import Header from '@components/Layout/Header'
 const Layout: React.FC<{ user: any }> = ({ children, user }) => {
   return (
     <div>
+      <style>
+        {`
+        .dropdown-menu {
+          right: 0;
+          left: auto;
+        }
+        `}
+      </style>
       <Header user={user} />
       {children}
     </div>
