26adc19944358bc6d0760fe80ef29aa2c92b7cde
파랑이 <parangee9706@gmail.com>
2021-03-16 17:19:50 +0900

    feat: implement public route

--- src/auth/jwt.guard.ts
+++ src/auth/jwt.guard.ts
@@ -1,7 +1,26 @@
 import { AuthGuard } from '@nestjs/passport'
-import { SetMetadata } from '@nestjs/common'
+import { ExecutionContext, SetMetadata } from '@nestjs/common'
+import { Reflector } from '@nestjs/core'
+import { Observable } from 'rxjs'
 
-export class JwtGuard extends AuthGuard(['jwt']) {}
+export class JwtGuard extends AuthGuard(['jwt']) {
+  constructor(private reflector: Reflector) {
+    super()
+  }
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ])
+    if (isPublic) {
+      return true
+    }
+    return super.canActivate(context)
+  }
+}
 
 export const IS_PUBLIC_KEY = 'isPublic'
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true)
