c28109f39d3d56ef27c1fd845564a96d933b78a5
파랑이 <parangee9706@gmail.com>
2021-03-17 22:00:05 +0900

    feat: implement breadcrumb

--- components/PageBase.tsx
+++ components/PageBase.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Breadcrumb } from 'react-bootstrap-v5'
+import Link from 'next/link'
 
 const PageBase: React.FC<{
   title: string
@@ -7,10 +9,17 @@ const PageBase: React.FC<{
     path: string
     as: string
   }[]
-}> = ({ title }) => {
+}> = ({ title, steps }) => {
   return (
     <div>
       <h2>{title}</h2>
+      <Breadcrumb>
+        {steps.map((r, i) => (
+          <Link href={r.path} as={r.as} key={i}>
+            <Breadcrumb.Item>{r.name}</Breadcrumb.Item>
+          </Link>
+        ))}
+      </Breadcrumb>
     </div>
   )
 }
