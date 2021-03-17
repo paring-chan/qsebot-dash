c8d28e3f1cec10f10395063dd50b073d74738760
파랑이 <parangee9706@gmail.com>
2021-03-17 15:45:18 +0900

    feat: render

--- src/password/password.controller.ts
+++ src/password/password.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, Render } from '@nestjs/common'
 
 @Controller('password')
-export class PasswordController {}
+export class PasswordController {
+  @Get()
+  @Render('password')
+  root() {
+    return {}
+  }
+}
