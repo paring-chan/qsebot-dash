76e5aa249d85815628229c2ff16f3ca5a52035a5
파랑이 <parangee9706@gmail.com>
2021-03-17 15:38:16 +0900

    feat: password controller

--- src/password/password.module.ts
+++ src/password/password.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { PasswordController } from './password.controller';
 
-@Module({})
+@Module({
+  controllers: [PasswordController]
+})
 export class PasswordModule {}
