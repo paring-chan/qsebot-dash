6faf8c85447c41734778f68302edf327e875690d
파랑이 <parangee9706@gmail.com>
2021-03-18 17:00:43 +0900

    feat: password check

--- src/password/password.controller.ts
+++ src/password/password.controller.ts
@@ -1,8 +1,12 @@
 import { Controller, Get, Post, Render, Req, Res } from '@nestjs/common'
 import { Request, Response } from 'express'
+import { UsersService } from '../users/users.service'
+import bcrypt from 'bcrypt'
 
 @Controller('password')
 export class PasswordController {
+  constructor(private userService: UsersService) {}
+
   @Get()
   @Render('password')
   root() {
@@ -10,7 +14,20 @@ export class PasswordController {
   }
 
   @Post()
-  update(@Req() _req: Request, @Res() res: Response) {
+  async update(@Req() req: Request, @Res() res: Response) {
+    const user = await this.userService.getUser((req.user as any).id)
+    if (!user) return res.redirect('/')
+    const { currentPassword, newPassword, newPasswordConfirm } = req.body
+    if (!(await bcrypt.compare(currentPassword, user.password))) {
+      return res.render('password', {
+        flash: '현재 비밀번호가 일치하지 않습니다',
+      })
+    }
+    if (newPassword !== newPasswordConfirm) {
+      return res.render('password', {
+        flash: '새 비밀번호가 일치하지 않습니다.',
+      })
+    }
     res.render('Index', {
       flash: '비밀번호가 변경되었습니다.',
     })
--- src/password/password.module.ts
+++ src/password/password.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
 import { PasswordController } from './password.controller'
+import { UsersModule } from '../users/users.module'
 
 @Module({
   controllers: [PasswordController],
+  imports: [UsersModule],
 })
 export class PasswordModule {}
