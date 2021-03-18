17d53db633bee6351eb5f3dc3d7668ac5643478f
파랑이 <parangee9706@gmail.com>
2021-03-18 21:13:14 +0900

    feat: pass user list

--- src/accounts/accounts.controller.ts
+++ src/accounts/accounts.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get, Render } from '@nestjs/common'
+import knex from '@utils/knex'
 
 @Controller('accounts')
 export class AccountsController {
   @Get()
   @Render('accounts')
   async accounts() {
-    return {}
+    return {
+      accounts: await knex('users').select('id'),
+    }
   }
 }
