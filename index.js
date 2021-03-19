4207b76fefd196f447ae913f01530e5649b29697
파랑이 <parangee9706@gmail.com>
2021-03-19 16:15:39 +0900

    feat: server

---
+++ index.js
@@ -0,0 +1,17 @@
+const AdminBro = require('admin-bro')
+const AdminBroExpress = require('@admin-bro/express')
+
+const express = require('express')
+const app = express()
+
+const adminBro = new AdminBro({
+  databases: [],
+  rootPath: '/admin',
+})
+
+const router = AdminBroExpress.buildRouter(adminBro)
+
+const app = require('express')()
+
+app.use(adminBro.options.rootPath, router)
+app.listen(process.env.PORT)
